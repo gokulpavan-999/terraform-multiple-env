96592c3f3e63521618b8383b3fac56eed48ff5fe
gokulpavan-999 <pavanthota480@gmail.com>
2025-10-21 15:35:48 +0530

    Update provider.tf

--- workspaces/provider.tf
+++ workspaces/provider.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "s3" {
     bucket = "remote-state-pavan"
-    key = "dynamic-block-demo"
+    key = "workspace-demo"
     region = "us-east-1"
     use_lockfile = true 
     encrypt = true 
